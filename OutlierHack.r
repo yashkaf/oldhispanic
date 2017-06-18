3b87c55218540ee798a36e818bd429e6cd3d1842
Yashkaf <yashkaf@gmail.com>
2017-06-18 14:50:04 -0400

    Create OutlierHack.r

---
+++ OutlierHack.r
@@ -0,0 +1,29 @@
+outlier_hack=function(n=30, hacks=1) {
+  # n is the sample size, hacks is the number of outliers to remove
+  x=rnorm(n)
+  y=rnorm(n)
+  # Two independent variables
+  for(i in 1:hacks) {
+    z=x*y
+    if(cor(x,y)>0) {
+      x=x[-which.min(z)]
+      y=y[-which.min(z)]
+    }
+    else {
+      x=x[-which.max(z)]
+      y=y[-which.max(z)]
+    }
+  }
+  m=lm(y~x)
+  return(summary(m)$fstatistic)
+}
+
+hackability=function(n=30, hacks=1, sims=1000, alpha=0.05) {
+  fstats=numeric(0)
+  # Linear models in R return F-statistics and not p-values directly
+  for(i in 1:sims) fstats=rbind(fstats,outlier_hack(n,hacks)[1])
+  threshold=qf(alpha,1,n-hacks-2,0,FALSE)
+  # Converting a p-value threshold (alpha) to an F-statistic threshold
+  return(length(fstats[fstats>threshold])/sims)
+  # This is the number of trials that were p-hacked to the alpha threshold
+}
