009133c5f3ba39fc8addd9eea519f87518a9133f
Yashkaf <yashkaf@gmail.com>
2017-06-18 14:52:13 -0400

    Update OutlierHack.r

--- OutlierHack.r
+++ OutlierHack.r
@@ -5,6 +5,7 @@ outlier_hack=function(n=30, hacks=1) {
   # Two independent variables
   for(i in 1:hacks) {
     z=x*y
+  # If x and y are positively correlated we remove the negatively correlated outliers, and vice versa.
     if(cor(x,y)>0) {
       x=x[-which.min(z)]
       y=y[-which.min(z)]
@@ -16,9 +17,11 @@ outlier_hack=function(n=30, hacks=1) {
   }
   m=lm(y~x)
   return(summary(m)$fstatistic)
+  # Return the F-statistic of regressing y on x with the outliers removed.
 }
 
 hackability=function(n=30, hacks=1, sims=1000, alpha=0.05) {
+  # sims is the number of simulations to run, alpha is the original false-positive threshold
   fstats=numeric(0)
   # Linear models in R return F-statistics and not p-values directly
   for(i in 1:sims) fstats=rbind(fstats,outlier_hack(n,hacks)[1])
